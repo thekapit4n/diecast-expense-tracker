49003871156217a9dc66dda1d6756f988b7aed79
Haiqal Halim <haiqal.halim@daysmart.com>
2026-08-30 22:41:45 +0800

    Revert "Reduce chart hover text to 10px"
    This reverts commit c6e94323e0b510d48004fe1304f582bd23856b61.

--- components/charts/sorted-bar-chart.tsx
+++ components/charts/sorted-bar-chart.tsx
@@ -141,7 +141,7 @@ export function SortedBarChart({
     })
 
     const axisTooltip = am5.Tooltip.new(root, { themeTags: ["axis"] })
-    axisTooltip.label.setAll({ fontSize: 10 })
+    axisTooltip.label.setAll({ fontSize: 12 })
 
     const yAxis = chart.yAxes.push(
       am5xy.CategoryAxis.new(root, {
@@ -177,7 +177,7 @@ export function SortedBarChart({
       pointerOrientation: "left",
       labelText: "{tooltip}",
     })
-    seriesTooltip.label.setAll({ fontSize: 10 })
+    seriesTooltip.label.setAll({ fontSize: 12 })
 
     const series = chart.series.push(
       am5xy.ColumnSeries.new(root, {
